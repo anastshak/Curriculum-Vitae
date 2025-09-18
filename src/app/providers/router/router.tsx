2903539690d1f7d19a1a7bc032ee2a6fbf1c0e40
anastshak <nastyashakura@gmail.com>
2025-09-18 17:01:37 +0300

    feat(router): update router with the latest changes

--- src/app/providers/router/router.tsx
+++ src/app/providers/router/router.tsx
@@ -1,10 +1,46 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Navigate } from 'react-router-dom';
 
+import { ProtectedRoute } from '@app/providers/router/routes/ProtectedRoute';
+import { PublicRoute } from '@app/providers/router/routes/PublicRoute';
 import App from '@pages/App';
+import { AuthLayout, LoginPage, SignupPage } from '@pages/auth';
+import { ROUTES } from '@shared/consts/routes';
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: <Navigate to={ROUTES.AUTH.LOGIN} replace />,
+  },
+  {
+    element: <ProtectedRoute />,
+    children: [
+      {
+        path: ROUTES.HOME,
+        element: <App />,
+      },
+    ],
+  },
+  {
+    element: <PublicRoute />,
+    children: [
+      {
+        path: ROUTES.AUTH.ROOT,
+        element: <AuthLayout />,
+        children: [
+          {
+            path: ROUTES.AUTH.LOGIN,
+            element: <LoginPage />,
+          },
+          {
+            path: ROUTES.AUTH.SIGNUP,
+            element: <SignupPage />,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: <div>Page not found</div>,
   },
 ]);
