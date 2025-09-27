83183307ae8d6bed984351b22603a9119ddc8408
anastshak <nastyashakura@gmail.com>
2025-09-28 00:03:09 +0300

    feat(users): add Searchbar

---
+++ src/features/users/ui/UsersTableSearchbar.tsx
@@ -0,0 +1,29 @@
+import { Search } from '@mui/icons-material';
+import { Box, InputAdornment, TextField } from '@mui/material';
+
+interface Props {
+  search: string;
+  setSearch: (value: string) => void;
+}
+
+export const UsersTableSearchbar = ({ search, setSearch }: Props) => (
+  <Box sx={{ display: 'flex', justifyContent: 'space-between', my: 2, ml: 3 }}>
+    <TextField
+      variant="outlined"
+      placeholder="Search"
+      size="small"
+      value={search}
+      onChange={(event) => setSearch(event.target.value)}
+      slotProps={{
+        input: {
+          startAdornment: (
+            <InputAdornment position="start">
+              <Search />
+            </InputAdornment>
+          ),
+        },
+      }}
+      sx={{ width: 320, '& .MuiOutlinedInput-root': { borderRadius: '40px', height: '40px' } }}
+    />
+  </Box>
+);
