7937f3bc2fc33ebadc1a0ac22660fd011f730a84
anastshak <nastyashakura@gmail.com>
2025-10-08 17:45:54 +0300

    feat(profile skills): create Mastery select

---
+++ src/entities/mastery/model/types.ts
@@ -0,0 +1,5 @@
+export type Props = {
+  value: string;
+  onChange?: (value: string) => void;
+  loading: boolean;
+};
---
+++ src/entities/mastery/ui/MasterySelect.tsx
@@ -0,0 +1,27 @@
+import { useTranslation } from 'react-i18next';
+
+import { BaseSelect } from '@shared/ui/BaseSelect';
+
+import { Props } from '../model/types';
+
+export const MasterySelect = ({ value, onChange, loading }: Props) => {
+  const { t } = useTranslation();
+
+  const options = [
+    { id: 'Expert', label: t('skills.masteryLevels.expert') },
+    { id: 'Proficient', label: t('skills.masteryLevels.proficient') },
+    { id: 'Competent', label: t('skills.masteryLevels.competent') },
+    { id: 'Advanced', label: t('skills.masteryLevels.advanced') },
+    { id: 'Novice', label: t('skills.masteryLevels.novice') },
+  ];
+
+  return (
+    <BaseSelect
+      label={t('formFields.skillMastery')}
+      value={value}
+      onChange={onChange}
+      loading={loading}
+      options={options}
+    />
+  );
+};
